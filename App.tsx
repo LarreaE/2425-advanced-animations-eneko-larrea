4759dc857c3ecf93f2544fe87a7b151603aaab9a
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-10-10 12:55:37 +0200

    [ADD] added backdrop

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useRef} from "react";
-import { FlatList , Animated} from "react-native";
+import { FlatList , Animated, View, Image} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
 import styled from "styled-components/native";
@@ -7,21 +7,26 @@ import Rating from "./components/Rating";
 import Genre from "./components/Genre";
 import { getMovies } from "./api"
 import * as CONSTANTS from "./constants/constants"
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 
 const Container = styled.View`
     flex: 1;
+    padding-top: 50px;
+    background-color: #000;
 `
 
 const PosterContainer = styled.View`
     width: ${CONSTANTS.ITEM_SIZE}px;
+    margin-top: ${CONSTANTS.TOP}px;
 `
 
 const Poster = styled.View`
     margin-horizontal: ${CONSTANTS.SPACING}px;
     padding: ${CONSTANTS.SPACING * 2}px;
     align-items: center;
-    background-color: #FFFFFF;
+    background-color: rgba(255 ,255 ,255 ,0.1);
     border-radius: 10px;
 `
 const PosterImage = styled.Image`
@@ -36,15 +41,35 @@ const PosterImage = styled.Image`
 const PosterTitle = styled.Text`
     font-family: Syne-Mono;
     font-size: 18px;
+    color: #FFF;
 `
 
 const PosterDescription = styled.Text`
     font-family: Syne-Mono;
     font-size: 12px;
+    color: #FFF;
 `
 
 const DummyContainer = styled.View`
     width: ${CONSTANTS.SPACER_ITEM_SIZE}px;
+`
+
+const ContentContainer = styled.View`
+    position: absolute;
+    width: ${CONSTANTS.WIDTH}px;
+    height: ${CONSTANTS.BACKDROP_HEIGHT}px;
+`
+
+const BackdropContainer = styled.View`
+    width: ${CONSTANTS.WIDTH}px;
+    position: absolute;
+    height: ${CONSTANTS.BACKDROP_HEIGHT}px;
+    overflow: hidden;
+`
+const BackdropImage = styled.Image`
+    position: absolute;
+    width: ${CONSTANTS.WIDTH}px;
+    height: ${CONSTANTS.BACKDROP_HEIGHT}px;
 `
 const scrollX = useRef(new Animated.Value(0)).current
 
@@ -64,12 +89,53 @@ export default function App() {
     let [fontLoaded] = useFonts({
         'Syne-Mono' : require('./assets/fonts/SyneMono-Regular.ttf')
     });
+
+    const Backdrop = ({movies, scrollX}) => {
+        return (
+            <ContentContainer>
+                <FlatList 
+                    data={movies}
+                    keyExtractor={item => `${item.key}-back`}
+                    removeClippedSubviews={false}
+                    contentContainerStyle={{width: CONSTANTS.WIDTH, height: CONSTANTS.BACKDROP_HEIGHT}}
+                    renderItem={({item, index}) => {
+                        if (!item.backdropPath) {
+                            return null
+                        }
+                        const translateX = scrollX.interpolate({
+                            inputRange: [(index - 1) * CONSTANTS.ITEM_SIZE, index*CONSTANTS.ITEM_SIZE],
+                            outputRange: [0, CONSTANTS.WIDTH]
+                        })
+                        return (
+                            <BackdropContainer
+                                as={Animated.View}
+                                style={{transform: [{translateX}]}}
+                            >
+                                <BackdropImage source={{uri: item.backdropPath}} />
+                            </BackdropContainer>
+                        )
+                    }}
+                />
+                <LinearGradient 
+                    colors={['rgba(0, 0, 0, 0', 'black']}
+                    style={{
+                        height: CONSTANTS.BACKDROP_HEIGHT,
+                        width: CONSTANTS.WIDTH,
+                        position: 'absolute',
+                        bottom: 0,
+                    }}
+                />
+            </ContentContainer>
+        )
+    }
+
     if (!loaded) {
         return <PosterDescription>App Loading...</PosterDescription>
     }
 
     return (
         <Container>
+            <Backdrop movies={movies} scrollX={scrollX} />
             <StatusBar />
             <Animated.FlatList
                 onScroll={Animated.event(
